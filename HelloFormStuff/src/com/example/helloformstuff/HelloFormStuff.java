07a7754c56379ba40afe21ef308e7a788bbac937
ngbrown <nathan@nkbrown.us>
2011-01-30 16:48:04 -0600

    Added edit text section.

--- HelloFormStuff/src/com/example/helloformstuff/HelloFormStuff.java
+++ HelloFormStuff/src/com/example/helloformstuff/HelloFormStuff.java
@@ -2,9 +2,12 @@ package com.example.helloformstuff;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class HelloFormStuff extends Activity {
@@ -19,6 +22,19 @@ public class HelloFormStuff extends Activity {
 			public void onClick(View v) {
 				// Perform action on clicks
 				Toast.makeText(HelloFormStuff.this, "Beep Bop", Toast.LENGTH_SHORT).show();
+			}
+		});
+        
+        final EditText edittext = (EditText)findViewById(R.id.edittext);
+        edittext.setOnKeyListener(new OnKeyListener() {
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				// If the event is a key-down event on the "enter" button
+				if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+						(keyCode == KeyEvent.KEYCODE_ENTER)) {
+					// Perform action on key press
+					Toast.makeText(HelloFormStuff.this, edittext.getText(), Toast.LENGTH_SHORT).show();
+				}
+				return false;
 			}
 		});
     }
