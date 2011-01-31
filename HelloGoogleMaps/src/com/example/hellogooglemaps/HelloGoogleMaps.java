c76d8873bee25f6b58bfbece277d9470f0ef1890
ngbrown <nathan@nkbrown.us>
2011-01-30 20:16:35 -0600

    Second stage of HelloGoogleMaps.  Added marker.

--- HelloGoogleMaps/src/com/example/hellogooglemaps/HelloGoogleMaps.java
+++ HelloGoogleMaps/src/com/example/hellogooglemaps/HelloGoogleMaps.java
@@ -1,9 +1,15 @@
 package com.example.hellogooglemaps;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 public class HelloGoogleMaps extends MapActivity {
     /** Called when the activity is first created. */
@@ -14,6 +20,16 @@ public class HelloGoogleMaps extends MapActivity {
         
         MapView mapView = (MapView)findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        List<Overlay> mapOverlays = mapView.getOverlays();
+        Drawable drawable = this.getResources().getDrawable(R.drawable.androidmarker);
+        HelloItemizedOverlay itemizedoverlay = new HelloItemizedOverlay(drawable, this);
+        
+        GeoPoint point = new GeoPoint(19240000,-99120000);
+        OverlayItem overlayitem = new OverlayItem(point, "¡Hola, Mundo!", "I'm in Mexico City!");
+        
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
     }
 
 	@Override
