5d9f5d4c0ea07e924a805e269791d6ca24145793
ngbrown <nathan@nkbrown.us>
2011-01-30 19:53:27 -0600

    Added HelloGoogleMaps project.

---
+++ HelloGoogleMaps/src/com/example/hellogooglemaps/HelloGoogleMaps.java
@@ -0,0 +1,23 @@
+package com.example.hellogooglemaps;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class HelloGoogleMaps extends MapActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        MapView mapView = (MapView)findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		return false;
+	}
+}
