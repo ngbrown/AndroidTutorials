da33019ca71823c16523d50bb8f2fd19d7d7da28
ngbrown <nathan@nkbrown.us>
2011-01-30 20:44:13 -0600

    Finished HelloWebView demo.  Added ability to handle links internally.

--- HelloWebView/src/com/example/hellowebview/HelloWebView.java
+++ HelloWebView/src/com/example/hellowebview/HelloWebView.java
@@ -2,7 +2,9 @@ package com.example.hellowebview;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class HelloWebView extends Activity {
 	WebView mWebView;
@@ -15,6 +17,24 @@ public class HelloWebView extends Activity {
         
         mWebView = (WebView)findViewById(R.id.webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new HelloWebViewClient());
         mWebView.loadUrl("http://www.google.com");
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    	if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+    		mWebView.goBack();
+    		return true;
+    	}
+    	return super.onKeyDown(keyCode, event);
+    }
+    
+    private class HelloWebViewClient extends WebViewClient {
+    	@Override
+    	public boolean shouldOverrideUrlLoading(WebView view, String url) {
+    		view.loadUrl(url);
+    		return true;
+    	}
     }
 }
