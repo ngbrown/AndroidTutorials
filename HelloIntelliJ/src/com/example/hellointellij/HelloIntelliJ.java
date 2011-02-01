dae75fb6165f1af457f65b0ad06aeabf890b3bf2
ngbrown <nathan@nkbrown.us>
2011-01-31 21:58:20 -0600

    Added new IntelliJ IDEA based project.

---
+++ HelloIntelliJ/src/com/example/hellointellij/HelloIntelliJ.java
@@ -0,0 +1,15 @@
+package com.example.hellointellij;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloIntelliJ extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
