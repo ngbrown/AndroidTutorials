d287ec7d299e35d1ab76421a2e45df1c0cccbf39
ngbrown <nathan@nkbrown.us>
2011-02-02 00:11:22 -0600

    Finished part 1 of the tutorial.

--- Notepad/src/com/android/demo/notepad1/Notepadv1.java
+++ Notepad/src/com/android/demo/notepad1/Notepadv1.java
@@ -16,29 +16,64 @@
 
 package com.android.demo.notepad1;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleCursorAdapter;
 
-public class Notepadv1 extends Activity {
+public class Notepadv1 extends ListActivity {
+    public static final int INSERT_ID = Menu.FIRST;
     private int mNoteNumber = 1;
+    private NotesDbAdapter mDbHelper;
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.notepad_list);
+        mDbHelper = new NotesDbAdapter(this);
+        mDbHelper.open();
+        fillData();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Auto-generated method stub
-        return super.onCreateOptionsMenu(menu);
+        boolean result = super.onCreateOptionsMenu(menu);
+        menu.add(0, INSERT_ID, 0, R.string.menu_insert);
+        return result;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case INSERT_ID:
+                createNote();
+                return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void createNote() {
+        String noteName = "Note " + mNoteNumber++;
+        mDbHelper.createNote(noteName, "");
+        fillData();
+    }
+
+    private void fillData() {
+        // Get all of the notes from the database and create the item list
+        Cursor c = mDbHelper.fetchAllNotes();
+        startManagingCursor(c);
+
+        String[] from = new String[]{NotesDbAdapter.KEY_TITLE};
+        int[] to = new int[]{R.id.text1};
+
+        // Now create an array adapter and set it to display using our row
+        SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.notes_row, c, from, to);
+        setListAdapter(notes);
     }
 }
